af2aa4bb7072f00f92eb4726108cc1e3fc20ae8c
sleatherv <sleatherv28@hotmail.com>
2022-03-15 22:39:33 -0500

    [ADD] Provider as state handler

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:qr_reader/providers/ui_provider.dart';
 import 'package:qr_reader/screens/screens.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Material App',
-      initialRoute: 'home',
-      routes: {
-        'home': (_) => HomeScreen(),
-        'maps': (_) => MapScreen(),
-      },
-      theme: ThemeData(
-        // primaryColor: Colors.deepPurple,
-        primarySwatch: Colors.deepPurple
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_)=> UiProvider()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Material App',
+        initialRoute: 'home',
+        routes: {
+          'home': (_) => HomeScreen(),
+          'maps': (_) => MapScreen(),
+        },
+        theme: ThemeData(
+          // primaryColor: Colors.deepPurple,
+          primarySwatch: Colors.deepPurple
+        ),
       ),
     );
   }
