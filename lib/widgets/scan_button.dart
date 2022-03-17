f3f5c3d817f3a597e0c320b220b86c6bb8e78d88
sleatherv <sleatherv28@hotmail.com>
2022-03-16 21:18:19 -0500

    [ADD] ScanModel

--- lib/widgets/scan_button.dart
+++ lib/widgets/scan_button.dart
@@ -10,11 +10,13 @@ class ScanButton extends StatelessWidget {
       elevation: 0,
       child: const Icon(Icons.filter_center_focus),
       onPressed: () async{
-        String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
-                                                    '#3D8BEF', 
-                                                    'Cancelar', 
-                                                    false, 
-                                                    ScanMode.QR);
+        final barcodeScanRes = 'https://pub.dev/packages/flutter_barcode_scanner';
+
+        // String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+        //                                             '#3D8BEF', 
+        //                                             'Cancelar', 
+        //                                             false, 
+        //                                             ScanMode.QR);
         print(barcodeScanRes);
       },
     );
