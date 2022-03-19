a08d222d6f7706edd0ea1122334acb31bba6b722
sleatherv <sleatherv28@hotmail.com>
2022-03-18 22:03:05 -0500

    Show Scans UI - No database

--- lib/screens/maps_screen.dart
+++ lib/screens/maps_screen.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
 
 class MapsScreen extends StatelessWidget {
-   
+
   const MapsScreen({Key? key}) : super(key: key);
   
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Maps Screen'),
+    return  ListView.builder(
+      itemCount: 10,
+      itemBuilder: (_, i) => ListTile(
+        leading: Icon(Icons.map, color: Theme.of(context).primaryColor),
+        title: const Text('http://asjsdalfs.com'),
+        subtitle: const Text('ID: 1'),
+        trailing: const Icon(Icons.keyboard_arrow_right, color: Colors.grey),
+        onTap: () => print('open somenthing...') ,
+      ),
+    
     );
   }
 }
