ff75138569414924a0798db4c9c4bd4913c5177f
sleatherv <sleatherv28@hotmail.com>
2022-03-23 22:09:03 -0500

    Config the map first time

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,20 +1,40 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:qr_reader/providers/db_provider.dart';
 import 'package:qr_reader/models/scan_model.dart';
-class MapScreen extends StatelessWidget {
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class MapScreen extends StatefulWidget {
   const MapScreen({Key? key}) : super(key: key);
-  
+
+  @override
+  State<MapScreen> createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+
+  Completer<GoogleMapController> _controller = Completer();
+
   @override
   Widget build(BuildContext context) {
+    final CameraPosition initialPoint = CameraPosition(
+      target: LatLng(37.42796133580664, -122.085749655962),
+      zoom: 14.4746,
+    );
     final ScanModel scan = ModalRoute.of(context)!.settings.arguments as ScanModel; //Read the scan from argument
 
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
-        title: Text('Coordenadas'),
+        title: const Text('Coordenadas'),
       ),
-      body: Center(
-        child: Text(scan.value),
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: initialPoint,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
       ),
     );
   }
